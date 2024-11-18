4320b2fcd9395cc0f201a08542a6ae74bc133f9e
chjawad1 <chjawad1152@gmail.com>
2024-11-18 10:39:56 +0500

    create icons and Text widgets seprately
     Creating a Reusable Icon Widget
    Let's create a reusable Icon widget that can take parameters for the icon type, color, and size RepeatContainerCode: Handles the reusable container logic.
    CardWidget: Displays the icon and label.

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -1,80 +1,22 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() {
-  runApp(BMICalculatorApp());
+  runApp(BMICalculator());
 }
 
-class BMICalculatorApp extends StatelessWidget {
+class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-      title: 'BMI Calculator',
-      home: BMICalculatorScreen(),
-    );
-  }
-}
-
-class BMICalculatorScreen extends StatefulWidget {
-  @override
-  _BMICalculatorScreenState createState() => _BMICalculatorScreenState();
-}
-
-class _BMICalculatorScreenState extends State<BMICalculatorScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calculator'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: repeatcontainercode(
-                    colors: Color(0xFF1D1E33),
-                  ),
-                ),
-                Expanded(
-                  child: repeatcontainercode(colors: Color(0xFF1D1E33),),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: repeatcontainercode(colors: Color(0xFF1D1E33),),
-          ),
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: repeatcontainercode(colors: Colors.blue),
-                ),
-                Expanded(
-                  child: repeatcontainercode(colors: Color(0xFF1D1E33),),
-                ),
-              ],
-            ),
-          ),
-        ],
+      home: InputPage(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        // textTheme: TextTheme(bodySmall: TextStyle(color: Colors.white),
       ),
     );
   }
 }
 
-class repeatcontainercode extends StatelessWidget {
-  repeatcontainercode({required this.colors});
- final Color colors;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colors,
-        borderRadius: BorderRadius.circular(10.0)
-
-      ),
-    );
-  }
-}
