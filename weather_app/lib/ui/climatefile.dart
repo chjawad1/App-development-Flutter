b6f811c2d023c185e9f76338534e28cb1af6e799
chjawad1 <chjawad1152@gmail.com>
2024-11-25 16:54:41 +0500

    Video 10(Display data using widget)
    Display data using widget

--- weather_app/lib/ui/climatefile.dart
+++ weather_app/lib/ui/climatefile.dart
@@ -12,10 +12,11 @@ class climate extends StatefulWidget {
 }
 
 class _climateState extends State<climate> {
-  void showStuff() async{
-    Map data = await getWeather(util.apiId ,util.defaultCity);
-  print(data.toString());
-}
+  void showStuff() async {
+    Map data = await getWeather(util.apiId, util.defaultCity);
+    print(data.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,24 +26,24 @@ class _climateState extends State<climate> {
         actions: [
           IconButton(
             icon: Icon(Icons.menu),
-            onPressed: ()=>showStuff(),
+            onPressed: () => showStuff(),
           )
         ],
       ),
       body: Stack(
         children: [
           Center(
-            child: Image(image: AssetImage('images/umbrella.jpeg'),
-              height: 1200.0,
-              width: 590.0,
-              fit: BoxFit.fill,
-            )
+              child: Image(image: AssetImage('images/umbrella.jpeg'),
+                height: 1200.0,
+                width: 590.0,
+                fit: BoxFit.fill,
+              )
           ),
           Container(
             alignment: Alignment.topRight,
-            margin:  EdgeInsets.fromLTRB(0.0, 10.9,20.9,0.0),
-            child:Text(
-                'Vehari',
+            margin: EdgeInsets.fromLTRB(0.0, 10.9, 20.9, 0.0),
+            child: Text(
+              'Vehari',
               style: cityStyle(),
             ),
           ),
@@ -52,21 +53,63 @@ class _climateState extends State<climate> {
           ),
           Container(
 
-            margin:  EdgeInsets.fromLTRB(30.0, 390.0,20.9,0.0),
-            child:Text(
-              '50.32F',
-              style: tempStyle()
-            ),
+            margin: EdgeInsets.fromLTRB(30.0, 390.0, 20.9, 0.0),
+            child: updateTempWidget('Lahore'),
           ),
         ],
       ),
     );
   }
-  Future <Map> getWeather(String appId, String city)async{
+
+  Future <Map> getWeather(String appId, String city) async {
     String apiUrl =
-       ' https://api.openweathermap.org/data/2.5/weather?q=$city&appid=''${util.apiId}&units=imperial';
+        ' https://api.openweathermap.org/data/2.5/weather?q=$city&appid=''${util
+        .apiId}&units=imperial';
     http.Response response = await http.get(Uri.parse(apiUrl));
     return json.decode(response.body);
+  }
+
+  Widget updateTempWidget(String city) {
+    return FutureBuilder(
+      future: getWeather(util.apiId, city.isEmpty ? util.defaultCity : city),
+      builder: (BuildContext context, AsyncSnapshot<Map> snapshot) {
+        if (snapshot.hasData) {
+          Map content = snapshot.data!;
+          return Container(
+            margin: const EdgeInsets.fromLTRB(30.0, 250.0, 0.0, 0.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ListTile(
+                  title: Text(
+                    content['main']['temp'].toString() + "F",
+                    style: TextStyle(
+                      fontSize: 49.9,
+                      fontStyle: FontStyle.normal,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  subtitle: Text(
+                    "Humidity: ${content['main']['humidity'].toString()}\n"
+                        "Min: ${content['main']['temp_min'].toString()}F\n"
+                        "Max: ${content['main']['temp_max'].toString()}F\n",
+                    style: extraData(),
+                  ),
+                ),
+              ],
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Text(
+            'Error loading weather data',
+            style: TextStyle(color: Colors.red),
+          );
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
+    );
   }
 }
 
@@ -84,4 +127,8 @@ TextStyle tempStyle(){
     fontWeight: FontWeight.w500,
     fontSize: 49.9
   );
+}
+TextStyle extraData() {
+  return new TextStyle(
+      color: Colors.white70, fontStyle: FontStyle.normal, fontSize: 17.0);
 }
